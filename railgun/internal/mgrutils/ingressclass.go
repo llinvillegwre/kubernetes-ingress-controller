7d51acca5ca7acb5d2450a3080d4bd1440512a42
Shane Utt <shaneutt@users.noreply.github.com>
2021-04-29 10:46:11 -0400

    Update railgun/internal/mgrutils/ingressclass.go

--- railgun/internal/mgrutils/ingressclass.go
+++ railgun/internal/mgrutils/ingressclass.go
@@ -2,5 +2,5 @@ package mgrutils
 
 import "github.com/kong/kubernetes-ingress-controller/pkg/annotations"
 
-// IngressClass is the current "kubernetes.io/ingress.class" configured for controllers
+// IngressClass is the current ingress class name configured for controllers
 var IngressClass = annotations.DefaultIngressClass
